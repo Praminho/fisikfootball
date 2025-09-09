43f5dc4bd57902d8ae2313063f42b699b8842149
Praminho <prama.ardend@ui.ac.id>
2025-09-09 22:39:17 +0700

    update views untuk nama aplikasi

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'aplikasi : FisikFootball'
+        'aplikasi : FisikFootball', 
         'npm' : '2406439066',
         'name': 'Prama Ardend Narendradhipa',
         'class': 'PBP F'
